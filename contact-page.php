c7683560f497c2e4608922cebbc3a8f12488f91e
shakebu <shakeeb.pixako@gmail.com>
2022-11-11 19:47:39 +0500

    new pages templates added

---
+++ contact-page.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * The template for Contact page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package GHC
+ */
+
+/* Template Name: Contact Page */
+
+
+
+
+get_header();
+?>
+<div class="contact-section jumbotron jumbotron-fluid">
+    <div class="container py-5 my-5">
+        <div class="text-center">
+            <h1 class="h1 large-font font-white mb-4">Impressum</h1>
+        </div>
+    </div>
+</div>
+
+<section class="container py-5">
+    <div class=" row text-center">
+        <h2 class="text-medium col-lg-6 mx-auto">Impressum der GHC GmbH – Ihr Online Marketing Partner!</h2>
+        <p class="mx-auto">Wenn Sie weitere Fragen haben und Beratung wünschen, hinterlassen Sie bitte Ihre Daten und wir werden uns so schnell wie möglich mit Ihnen in Verbindung setzen.</p>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-6 justify-content-center align-self-center pr-md-5">
+            <h2 class="h2 font-medium text-danger">In Kontakt kommen</h2>
+            <h5 class="mb-3">Bei uns für jede Anfrage</h5>
+            <?php echo apply_shortcodes( '[contact-form-7 id="2483" title="Ohne Titel"]' ); ?>
+        </div>
+        <div class="col-md-6 justify-content-center align-self-center pl-md-5">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2712.228452852147!2d8.097964215136237!3d47.17296352582623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47901ff781c4e1b1%3A0x224a5fb7fdcdc535!2sJosef-M%C3%BCller-Weg%206%2C%206210%20Sursee%2C%20Switzerland!5e0!3m2!1sen!2s!4v1667548770976!5m2!1sen!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" onload="this.width=screen.width;this.height=screen.height/2;"></iframe>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-6 pr-md-5">
+            <p><span class="font-weight-bold">Adresse: </span> GHC GmbHJosef-Müller-Weg 66210 Sursee</p>
+            <p><span class="font-weight-bold">HR-Nummer: </span> CH-100.4.801.253-5</p>
+            <p><span class="font-weight-bold">MwSt.-Nummer: </span> CHE-379.703.451 MWST</p>
+            <p><span class="font-weight-bold">UID: </span> CHE-379.703.451</p>
+            <p><span class="font-weight-bold">D-U-N-S® Nummer: </span> 48-011-4026</p>
+        </div>
+        <div class="col-md-6 pr-md-5">
+            <p><span class="font-weight-bold">Phone: </span> +41 62 791 14 13</p>
+            <p><span class="font-weight-bold">Fax: </span> +41 44 575 30 83</p>
+            <p><span class="font-weight-bold">Email: </span> gh @ ghc-gmbh.ch</p>
+        </div>
+    </div>
+
+    <div class="text-center">
+            <h4>Unterstützt durch</h4>
+            <a href="https://stadtbranche.ch/thema-online-marketing" class="text-danger text-decoration-none">StadtBranche.ch Online-marketing Schweiz</a>
+    </div>
+</section>
+<?php
+get_footer();
+?>
---
+++ sales_funnel-page.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * The template for  sales funnel page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package GHC
+ */
+
+/* Template Name: Sales Funnel Page */
+
+
+
+
+get_header();
+?>
+<div class="sales-funnel-section jumbotron jumbotron-fluid">
+    <div class="container py-5 my-5">
+        <div class="text-center">
+            <h1 class="h1 large-font font-white mb-4">Sales Funnel – Get really good leads fast!</h1>
+        </div>
+    </div>
+</div>
+
+<section class="container py-5">
+    <div class="row">
+        <div class="col-md-6 justify-content-center align-self-center pr-md-5">
+            <h2 class="h2 font-medium mb-3">Sales Funnel – Schnell, richtig gute Leads gewinnen!</h2>
+            <p>Breite Marketing-Kanäle sind heute für die gezielte, qualitativ hochwertige Lead-Generierung einfach zu ungenau. Selbst wenn man Quantität erzeugen kann, stimmt die Qualität einfach nicht. Zu breit ist die Streuung. Die Wünsche für Leads werden heute auch immer spitzer, sodass man nur durch einen genauen Prozess zum Erfolg kommt. Dafür eignet sich der Sales Funnel bestens.</p>
+        </div>
+        <div class="col-md-6 justify-content-center align-self-center pl-md-5">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/funnel.jpg' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row text-center mt-5 px-5">
+        <h2 class="h2 font-medium mx-auto col-lg-6 mb-3">Spitzer Akquiseprozess mit präzisem Output</h2>
+        <p class="mx-auto">Hinter dem Sales – oder Marketing Funnel steckt der Gedanke für den Akquiseprozess einen Trichter aufzubauen, der, je enger er wird, die ungeeigneten Kandidaten von den geeigneten Kandidaten trennt. Und so lange die Interessenten aussiebt bis am Ende des Trichters genau die passende Essenz an Leads, die man für sein Vorhaben haben will, übrigbleibt. Das reduziert die Streuung und Kosten ungemein. Ausserdem ist der Aufbau eines Sales Funnels in der Regel einfacher und schneller durchzuführen.</p>
+        <p class="mx-auto">Im Prinzip geht es dabei darum, <a href="#" class="text-danger text-decoration-none font-weight-bold">Traffic</a> zu erzeugen, die die Kandidaten auf eine Landingpage holt und dort dafür sorgt, sie zu einer Anmeldung zu bewegen. Danach kann man diese in zahlende Kunden verwandeln. Die Wahrscheinlichkeit der Conversion bei diesen Interessenten ist sehr hoch. Da sie ja bereits bei der Suche in Google ihr Interesse gezeigt und durch die Anmeldung auf einer Landingpage den Willen zur Umsetzung bekundet haben.</p>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-6 justify-content-center align-self-center pr-md-5">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/aida.jpg' ?>" alt="">
+        </div>
+        <div class="col-md-6 justify-content-center align-self-center pl-md-5">
+            <h2 class="h2 font-medium mb-3">Oft verwendetes AIDA – Modell</h2>
+            <p>Für den Aufbau des Sales Funnels verwendet man oft die AIDA-Formel. Damit ist aber nicht das bekannte Kreuzfahrtschiff gemeint. 🙂</p>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+        <div class="col-md-6 pr-md-5">
+            <p>Die AIDA-Formel beschreibt ein Vorgehen im Marketing. Aus dem Englischen steht das für „Attention – Interest – Desire – Action“.</p>
+            <ul class="p-0 m-0 mb-3">
+                <li class="list-unstyled">„A“ steht für Attention – also Aufmerksamkeit erzeugen.</li>
+                <li class="list-unstyled">„I“ steht für Interesse wecken.</li>
+                <li class="list-unstyled">„D“ steht für „Desire“ – also den Wunsch nach „dem Haben wollen“ verstärken.</li>
+                <li class="list-unstyled">Und das letzte „A“ steht für die „Action“ – also den Verkaufsprozess, der dann abgeschlossen wird.</li>
+            </ul>
+            <p>Diese Formel kann man im Marketing generell anwenden, sie ist nicht auf das Online-Marketing beschränkt. Im klassischen <a href="#" class="text-danger font-weight-bold text-decoration-none">Marketing</a> erregt man die Aufmerksamkeit (A) zum Beispiel durch Plakate. Das Interesse (I) wird dann mit einer detaillierten Anzeige in einer Zeitschrift geweckt. Der Wunsch „D“ das Produkt haben zu wollen, wird durch die regelmässige Penetration an allen „Touch-Points“ erzeugt. Und zum Schluss wird der Verkauf „A“ über eine gezielte Offerte mit Bestellformular durchgeführt.</p>
+        </div>
+        <div class="col-md-6 pl-md-5">
+            <p>Im Online-Marketing, also z. B. bei einem Sales-Funnel, wird diese Prozesskette durch Methoden das Online-<a href="#" class="text-danger font-weight-bold text-decoration-none">Marketing</a> abgedeckt. Meist wird die Aufmerksamkeit durch Performance Ads (<a href="#" class="text-danger font-weight-bold text-decoration-none">Google Ads</a>) erzeugt, die in der Suche oder als Display im Internet eingeblendet wird. Die Interessenten werden damit auf eine <a href="#" class="text-danger font-weight-bold text-decoration-none">Landingpage</a> gelockt auf der, weiteres Interesse zu dem Thema geweckt wird. Mit einem Goody wird nun der Wunsch ein Produkt haben zu wollen erzeugt – Desire. Das kann, zum Beispiel, ein Formular sein, mit dem man einen kostenlosen Ratgeber bestellen kann. Hat der Kandidat seine persönlichen Daten (den Lead) abgesendet, kann man mittels E-Mail-Marketing den Kunden penetrieren bis er bestellt bzw. kauft – Action. Natürlich kann man dann diese Datenbasis für weitere Aktionen und Verkäufe mittels E-Mail-Marketing nutzen.</p>
+        </div>
+    </div>
+</section>
+
+<section class="py-5 bg-light">
+    <div class="container">
+        <div class="row text-center mb-4">
+            <h2 class="h2 font-medium col-lg-6 mx-auto">Mit Automatisierung Geld, Zeit und Kosten sparen</h2>
+            <p class="mx-auto px-5">Dank digitaler Lösungen lassen sich heute automatisierte <a href="#" class="text-danger font-weight-bold text-decoration-none">Sales-Funnel</a> im Online-Marketing aufbauen. Das heisst, man richtet einmal die entsprechenden Tools, nach der AIDA-Formel, ein und kann sie dann einfach laufen lassen. Macht man das richtig, kann man mit wenig Aufwand viele gute Leads und Verkäufe, also <a href="#" class="text-danger font-weight-bold text-decoration-none">Conversions</a> erzeugen. Das hört sich einfach an, oder? Natürlich kommt es, wie immer darauf an, wie man das macht.</p>
+        </div>
+        <div class="row">
+            <div class="col-md-6 pt-2">
+                <h4>Auf das „Wie“ kommt es an!</h4>
+                <p>Auch im Sales-Funnel kommt es auf die richtige Auswahl der Tools und dem richtigen Einsatz dieser Tools an. In Google Ads müssen die richtigen Anzeigen, Zielgruppen und Keywords eingerichtet und regelmässig optimiert werden. In der Landingpage kommt es auf viele Punkte an. Der Content muss stimmen, das Layout ist wichtig, der Teaser bzw. das Goody, zur Erzeugung vom Wunsch das Produkt haben zu wollen. Man muss den Interessenten zu einer Action bringen. Die Action muss getrackt und analysiert werden. Das E-Mail-Marketing muss clever aufgebaut sein. Über automatische Trigger und „wenn-dann“ Logik, kann man vorgefertigte E-Mails an den Interessenten senden und eine Interaktion bzw. einen Loop an Aktivitäten erzeugen. Selbst den Verkaufsprozess kann man zu einem Marketing-Tool nutzen.</p>
+            </div>
+            <div class="col-md-6 pt-2">
+                <h4>Nach dem Verkauf ist vor dem Verkauf!</h4>
+                <p>After-Sales Offerten, Gewinnspiele oder <a href="#" class="text-danger font-weight-bold text-decoration-none">Partner</a> integrieren. Wenn der Kunden uns schon einmal seine Aufmerksamkeit widmet, sollte man das immer ausnutzen. Also kann man noch weitere Offerten folgen lassen. Dabei kommt es auf das „Wie“ und „Wann“ an. Da alles digital ist, können die Daten auch gleich in ein CRM integriert werden, mit dem man weitere Interaktion und Kommunikation mit dem Kunden plant und durchführt. Nach dem Verkauf ist vor dem Verkauf – nur mit dem Unterschied, dass der Kunde schon von uns überzeugt ist.</p>
+            </div>
+            <div class="col-md-6 pt-2">
+                <h4>Kosten pro Lead senken!</h4>
+                <p>Durch einen gut aufgebauten Sales-Funnel kann man die <a href="#" class="text-danger font-weight-bold text-decoration-none">Kosten</a> für einen qualitativ hochwertigen Lead stark reduzieren und den Lead später noch leicht für andere Aktionen nutzen. Das heisst die TCO (Total Cost of Ownership) für den Lead sinkt und amortisiert sich immer mehr. Je nach dem in welcher Branche man sich befindet oder um welches Produkt es sich handelt, kann man mit einem Sales-Funnel um 50-60 % der Kosten sparen, die man sonst bei herkömmlichen Methoden eingesetzt hat. Gleichzeitig richtet man seine Aktivitäten spitz auf genau eine Richtung zu.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="py-5">
+    <div class="container">
+        <h2 class="h2 font-medium">Vorteile des Sales-Funnel</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="border-bottom border-danger py-2 h-100">
+                    Marketing wird einfacher
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="border-bottom border-danger py-2 h-100">
+                    Planung und Forecast werden genauer
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="border-bottom border-danger py-2 h-100">
+                    Conversion Raten steigen
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="border-bottom border-danger py-2 h-100">
+                    Zielgerichteter auf Action (Sales)
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="border-bottom border-danger py-2 h-100">
+                    Ergebnis: mehr Umsatz zu niedrigeren Kosten
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-red">
+    <div class="container">
+        <div class="row py-5 m-0 text-white">
+            <div class="col-lg-8 mx-auto text-center">
+                <h5 class="font-medium mb-4">Wir führen seit Jahren Sales-Funnels durch und setzen sie für unsere Kunden erfolgreich ein. Melden Sie sich bei uns, wenn Sie weitere Informationen benötigen oder ein Gespräch dazu führen möchten.</h4>
+                    <button class="btn theme-primary-button">In Kontakt kommen</button>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer();
+?>
+<script>
+    $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+        $(e.target)
+            .prev()
+            .find("i:last-child")
+            .toggleClass("fa-angel-down fa-angle-up");
+    });
+</script>
